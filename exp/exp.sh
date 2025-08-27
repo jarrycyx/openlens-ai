ff2bcde595f7acf99e045bdc04e74fd05e042d21
jarrycyx <dzdzzd@126.com>
2025-08-27 18:04:32 +0800

    feat(exp): 添加实验脚本并优化图形生成逻辑
    - 新增 exp.sh 脚本，用于启动实验
    - 修改 build_graph.py 中的图形生成逻辑，提高代码兼容性和稳定性

---
+++ exp/exp.sh
@@ -0,0 +1,5 @@
+python -m openlens_ai.build_graph \
+    --question "What is the prediction precision of AKI in ICU patients when dynamically predicting each day based on the past two days of historical data?" \
+    --dataset-path "dataset/mimic" \
+    --thread-id "pred_aki_dy_mimic" \
+    --email "dzdzzd@126.com"
--- openlens_ai/build_graph.py
+++ openlens_ai/build_graph.py
@@ -91,14 +91,14 @@ def build_graph(config: Config, checkpointer: InMemorySaver):
                 f.write(graph.get_graph(xray=True).draw_mermaid())
                 
             try:
-                graph_image = Image(graph.get_graph(xray=True).draw_mermaid_png())
+                graph_image = graph.get_graph(xray=True).draw_mermaid_png()
             except Exception as e:
                 error_info = traceback.format_exc()
                 logger.info(f'Failed to build mermaid graph: {e}')
-                graph_image = Image(graph.get_graph(xray=True).draw_png())
+                graph_image = graph.get_graph(xray=True).draw_png()
             # 保存
             with open(os.path.join(config.save_path, "overall_graph_image.png"), "wb") as f:
-                f.write(graph_image.data)
+                f.write(graph_image)
             # with open(os.path.join(config.save_path, "workspace", "graph_mermaid.txt"), "w") as f:
             #     f.write(graph.get_graph(xray=True).draw_mermaid())
         except Exception as e:
