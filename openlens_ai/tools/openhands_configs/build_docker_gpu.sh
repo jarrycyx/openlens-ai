f4ff683200b8726ec897dcade167d830690a9066
jarrycyx <dzdzzd@126.com>
2025-08-28 17:21:29 +0800

    更新openhands版本

--- openlens_ai/tools/openhands_configs/build_docker_gpu.sh
+++ openlens_ai/tools/openhands_configs/build_docker_gpu.sh
@@ -1,4 +1,5 @@
 docker build -t agent-med-gpu \
     -f openlens_ai/tools/openhands_configs/ExpDockerfile_gpu \
     --build-arg all_proxy="http://166.111.74.73:17890/" \
+    --progress=plain \
     .
