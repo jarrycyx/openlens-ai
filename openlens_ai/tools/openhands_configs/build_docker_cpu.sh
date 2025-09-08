d68095bc5789de037af0dafadd5338ddb99c48f6
jarrycyx <dzdzzd@126.com>
2025-09-08 18:13:10 +0800

    build(docker): 更新 Dockerfile 和构建脚本
    - 新增 ExpDockerfile_cpu 文件，用于构建 CPU 版本的镜像
    - 更新 ExpDockerfile_gpu 文件，换用 Ubuntu 20.04 基础镜像
    - 在两个 Dockerfile 中添加了更多 Python 依赖的安装
    - 新增 build_docker_cpu.sh 构建脚本，用于构建和保存 CPU 版本镜像
    - 更新 build_docker_gpu.sh 构建脚本，统一代理设置

--- openlens_ai/tools/openhands_configs/build_docker_cpu.sh
+++ openlens_ai/tools/openhands_configs/build_docker_cpu.sh
@@ -1,5 +1,5 @@
-docker build -t agent-med-gpu \
-    -f openlens_ai/tools/openhands_configs/ExpDockerfile_gpu \
+docker build -t agent-med-cpu \
+    -f openlens_ai/tools/openhands_configs/ExpDockerfile_cpu \
     --build-arg all_proxy="http://127.0.0.1:7890/" \
     --build-arg http_proxy="http://127.0.0.1:7890/" \
     --build-arg https_proxy="http://127.0.0.1:7890/" \
@@ -7,6 +7,6 @@ docker build -t agent-med-gpu \
     --progress=plain \
     .
 
-docker save agent-med-gpu | gzip > exp/docker/docker.tar.gz
+docker save agent-med-cpu | gzip > exp/docker/docker-cpu.tar.gz
 
-# gunzip -c exp/docker/docker.tar.gz | docker load
+# gunzip -c exp/docker/docker-cpu.tar.gz | docker load
